fb3a3eaffebcd78acc4399878e3f30cdfc9d10de
ourensubure-art <ourensubure@gmail.com>
2026-06-25 18:55:45 +0800

    feat: 接入管理端决策数据，新增任务详情页与确认反馈

---
+++ herder-config.js
@@ -0,0 +1,6 @@
+window.HERDER_CONFIG = {
+  HERDER_ID: "herder_demo",
+  PASTURE_ID: "pasture_001",
+  SUPABASE_URL: "https://jqubxwcjupapeaknfzpi.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_MndnE02o-5Q5xN7_rNyIhQ_1Ur7k8cq"
+};
